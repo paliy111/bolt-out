d82a199bd34a6b0f34d24dc24ec375b2962e82b7
paliy111 <pjoni1118@gmail.com>
2024-02-14 09:18:15 +0200

    made a new `GameObject` class

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 // JavaScript source code
-var x = 0;
-var y = 0;
-var vx = 0;
-var vy = 0;
 const gravity = 1;
+
+var char = {
+	img: new Image(),
+	w: 32,
+	h: 64,
+	x: 0,
+	dx: 0,
+	y: 0,
+	dy: 0,
+	
+	draw: (ctx, this_object) => {
+		ctx.drawImage(this_object.img, this_object.x, this_object.y, this_object.w, this_object.h);
+	}
+}
 
 var keysDown = {
     a: false,
@@ -11,12 +21,48 @@ var keysDown = {
     s: false,
     d: false,
 };
-var char = document.getElementById("char");
+
+class GameObject {
+	constructor(src, w, h) {
+		this.img = new Image();
+		this.img.src = src;
+		this.img.width = w;
+		this.img.height = h;
+		this.h = h;
+		this.w = w;
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	colides(other) {
+		return (this.x < other.x + other.w &&
+		this.x + this.w > other.x &&
+		this.y < other.y + other.h &&
+		this.y + this.h > other.y)
+	}
+	
+	moveTo(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	draw(ctx) {
+		ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+	}
+}
+
+
 
 function init() {
+	var new_char = new GameObject("Images/char.png", 32, 64);
+	char.img.src = "Images/char.png";
+	
+	ctx = document.getElementById("cvs").getContext("2d");
+	new_char.moveTo(200, 200);
+	new_char.draw(ctx);
     addEventListener("keydown", keyDownHandler);
     addEventListener("keyup", keyUpHandler);
-    window.requestAnimationFrame(drawFrame);
+    // window.requestAnimationFrame(drawFrame);
 }
 
 function keyDownHandler(e) {
@@ -52,32 +98,30 @@ function keyUpHandler(e) {
 function drawFrame() {
     ctx = document.getElementById("cvs").getContext("2d");
     ctx.clearRect(0, 0, 1280, 720);
-    ctx.drawImage(char, x, y);
+    char.draw(ctx, char);
     if (keysDown.a) {
-        vx = -3;
+        char.dx = -3;
     } else if (keysDown.d) {
-        vx = 3;
+        char.dx = 3;
     } else {
-        vx = 0;
+        char.dx = 0;
     }
 	
-	vy += gravity;
-    x += vx;
-    y += vy;
-    if (x < 0 || x + char.width > 1280) {
-        x -= vx;
-        vx = 0;
+	char.dy += gravity;
+    char.x += char.dx;
+    char.y += char.dy;
+    if (char.x < 0 || char.x + char.w > 1280) {
+        char.x -= char.dx;
+        char.dx = 0;
     }
 
-    if (y < 0 || y + char.height > 720) {
-        y -= vy;
-        vy = 0;
+    if (char.y < 0 || char.y + char.h > 720) {
+        char.y -= char.dy;
+        char.dy = 0;
 		if (keysDown.w) {
-        vy = -20;
+        char.dy = -20;
 		}
     }
 
     window.requestAnimationFrame(drawFrame);
 }
-
-init();
