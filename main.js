139a47ef43ed1c04083d803df0cde9ee2c76c7a7
paliy111 <pjoni1118@gmail.com>
2024-02-29 12:37:04 +0200

    updated movement system

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
 // JavaScript source code
-const gravity = 1;
+const gravity = 0.8;
 
-var char = {
-	img: new Image(),
-	w: 32,
-	h: 64,
-	x: 0,
-	dx: 0,
-	y: 0,
-	dy: 0,
-	
-	draw: (ctx, this_object) => {
-		ctx.drawImage(this_object.img, this_object.x, this_object.y, this_object.w, this_object.h);
-	}
-}
+var char;
+
+var blocks = new Array();
 
 var keysDown = {
     a: false,
@@ -49,56 +39,158 @@ class GameObject {
 	draw(ctx) {
 		ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
 	}
+	
+	drawHitbox(ctx) {
+		ctx.strokeStyle = "red";
+		ctx.strokeRect(this.x, this.y, this.w, this.h);
+	}
+}
+
+class Player extends GameObject {
+	constructor() {
+		super("Images/char.png", 32, 64);
+		this.dx = 0;
+		this.dy = 0;
+		this.ay = 0;
+		this.xRemainder = 0;
+		this.yRemainder = 0;
+		this.ground_hitbox = new GameObject("", 32, 8);
+		this.grounded = false;
+	}
+	
+	update(blocks) {
+		this.colided = false;
+		this.moveX(this.dx); // changes this.colided
+		this.moveY(this.dy);
+		this.ground_hitbox.moveTo(this.x, this.y + 60);
+		this.grounded = false;
+		for (var i = 0; i < blocks.length; i++) {
+			if (this.ground_hitbox.colides(blocks[i])){
+				this.grounded = true;
+				break;
+			}
+		}
+	}
+	
+	moveX(distance) {
+		this.xRemainder += distance;
+		var intMove = Math.round(this.xRemainder);
+		this.xRemainder -= intMove;
+		var direction = Math.sign(intMove);
+		while(intMove != 0) {
+			this.x += direction;
+			intMove -= direction;
+			for (var i = 0; i < blocks.length; i++) {
+				if (this.colides(blocks[i])) {
+					this.x -= direction;
+					this.colided = true;
+					return;
+				}
+			}
+		}
+	}
+	
+	moveY(distance) {
+		this.yRemainder += distance;
+		var intMove = Math.round(this.yRemainder);
+		this.yRemainder -= intMove;
+		var direction = Math.sign(intMove);
+		while(intMove != 0) {
+			this.y += direction;
+			intMove -= direction;
+			for (var i = 0; i < blocks.length; i++) {
+				if (this.colides(blocks[i])) {
+					this.y -= direction;
+					this.colided = true;
+					return;
+				}
+			}
+		}
+	}
+	
+	draw(ctx) {
+		super.draw(ctx);
+		this.ground_hitbox.drawHitbox(ctx);
+	}
 }
 
 
 
 function init() {
-	var new_char = new GameObject("Images/char.png", 32, 64);
-	char.img.src = "Images/char.png";
-	
-	ctx = document.getElementById("cvs").getContext("2d");
-	new_char.moveTo(200, 200);
-	new_char.draw(ctx);
+	char = new Player();
+	var block = new GameObject("Images/char.png", 16, 16);
+	blocks.push(block);
+	block.moveTo(600, 600);
     addEventListener("keydown", keyDownHandler);
     addEventListener("keyup", keyUpHandler);
-    // window.requestAnimationFrame(drawFrame);
+    window.requestAnimationFrame(drawFrame);
 }
 
 function keyDownHandler(e) {
-    if (e.key === "s") {
+	if (e.isComposing || e.keyCode === 229) {
+		return;
+	}
+	console.log(e.code);
+    if (e.code === "KeyS") {
         keysDown.s = true;
     }
-    else if (e.key === "w") {
+    else if (e.code === "KeyW") {
         keysDown.w = true;
     }
-    else if (e.key === "d") {
+    else if (e.code === "KeyD") {
         keysDown.d = true;
     }
-    else if (e.key === "a") {
+    else if (e.code === "KeyA") {
         keysDown.a = true;
     }
+	else if (e.code === "ArrowRight") {
+		keysDown.d = true;
+	}
+	else if (e.code === "ArrowLeft") {
+		keysDown.a = true;
+	}
+	else if (e.code === "ArrowUp") {
+		keysDown.w = true;
+	}
+	else if (e.code === "ArrowDown") {
+		keysDown.s = true;
+	}
 }
 
 function keyUpHandler(e) {
-    if (e.key === "s") {
+    if (e.code === "KeyS") {
         keysDown.s = false;
     }
-    else if (e.key === "w") {
+    else if (e.code === "KeyW") {
         keysDown.w = false;
     }
-    else if (e.key === "d") {
+    else if (e.code === "KeyD") {
         keysDown.d = false;
     }
-    else if (e.key === "a") {
+    else if (e.code === "KeyA") {
         keysDown.a = false;
     }
+	else if (e.code === "ArrowRight") {
+		keysDown.d = false;
+	}
+	else if (e.code === "ArrowLeft") {
+		keysDown.a = false;
+	}
+	else if (e.code === "ArrowUp") {
+		keysDown.w = false;
+	}
+	else if (e.code === "ArrowDown") {
+		keysDown.s = false;
+	}
 }
 
 function drawFrame() {
     ctx = document.getElementById("cvs").getContext("2d");
     ctx.clearRect(0, 0, 1280, 720);
-    char.draw(ctx, char);
+    char.draw(ctx);
+	for (var i = 0; i < blocks.length; i++) {
+		blocks[i].draw(ctx);
+	}
     if (keysDown.a) {
         char.dx = -3;
     } else if (keysDown.d) {
@@ -106,10 +198,23 @@ function drawFrame() {
     } else {
         char.dx = 0;
     }
-	
-	char.dy += gravity;
-    char.x += char.dx;
-    char.y += char.dy;
+	char.dy += char.ay;
+	if (char.dy > 15) {
+		char.dy = 15;
+	}
+    char.update(blocks);
+	if (char.grounded) {
+		char.ay = 0;
+		char.dy = 0;
+	} else {
+		char.ay = gravity;
+	}
+	if (char.colided) {
+		console.log("colided");
+	}
+	if (char.grounded && keysDown.w) {
+		char.dy = -20;
+	}
     if (char.x < 0 || char.x + char.w > 1280) {
         char.x -= char.dx;
         char.dx = 0;
@@ -119,7 +224,7 @@ function drawFrame() {
         char.y -= char.dy;
         char.dy = 0;
 		if (keysDown.w) {
-        char.dy = -20;
+			char.dy = -20;
 		}
     }
 
